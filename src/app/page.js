06e9cc58f84d6a59b2dd220ad53dd48c81fd636f
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-05-23 06:00:18 +0700

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Index from "@/app/index/Index";
+
+export default function Home() {
+  return (
+    <>
+      <Index/>
+    </>
+  );
+}
